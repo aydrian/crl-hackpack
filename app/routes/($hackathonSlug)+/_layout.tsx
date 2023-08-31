56497a75182c0e26caeee5308a8871c57fbbdb66
Aydrian Howard <aydrian@gmail.com>
2023-08-31 10:59:33 -0400

    chore: added social links to footer

--- app/routes/($hackathonSlug)+/_layout.tsx
+++ app/routes/($hackathonSlug)+/_layout.tsx
@@ -127,8 +127,74 @@ export default function Layout() {
         <Outlet />
       </main>
       <footer className="flex items-center justify-between bg-crl-deep-purple p-4 text-sm font-bold text-white">
-        <div className="flex flex-col text-xs">©2023 Cockroach Labs</div>
-        <div className="flex items-center gap-2">
+        <div className="flex basis-1/3 items-center gap-2 text-xs">
+          <a
+            href="https://instagram.com/cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="instagram" />
+            <span className="sr-only">Instagram</span>
+          </a>
+          <a
+            href="https://twitter.com/cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="twitter" />
+            <span className="sr-only">Twitter</span>
+          </a>
+          <a
+            href="https://youtube.com/cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="youtube" />
+            <span className="sr-only">YouTube</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/company/cockroach-labs/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="linkedin" />
+            <span className="sr-only">LinkedIn</span>
+          </a>
+          <a
+            href="https://www.reddit.com/r/cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="reddit" />
+            <span className="sr-only">Reddit</span>
+          </a>
+          <a
+            href="https://www.threads.net/@cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="threads" />
+            <span className="sr-only">Threads</span>
+          </a>
+          <a
+            href="https://github.com/cockroachdb"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icon className="h-6 w-6" name="github" />
+            <span className="sr-only">GitHub</span>
+          </a>
+        </div>
+        <div className="basis-1/3 text-center">
+          ©2023{" "}
+          <TrackingLink
+            className="hover:text-crl-electric-purple"
+            href="https://cockroachlabs.com"
+          >
+            Cockroach Labs
+          </TrackingLink>
+        </div>
+        <div className="flex basis-1/3 items-center justify-end gap-2">
           <a
             href="https://github.com/aydrian/crl-hackpack"
             rel="noreferrer"

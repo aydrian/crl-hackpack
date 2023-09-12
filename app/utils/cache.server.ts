aa8d74b54ba99a06f92460b51f7aeae7faf13ab9
Aydrian Howard <aydrian@gmail.com>
2023-09-12 11:12:29 -0400

    chore: add caching for social-preview and keep server running

---
+++ app/utils/cache.server.ts
@@ -0,0 +1,18 @@
+import {
+  type CacheEntry,
+  type CachifiedOptions,
+  cachified as baseCachified,
+  lruCacheAdapter
+} from "cachified";
+import { LRUCache } from "lru-cache";
+
+const lru = new LRUCache<string, CacheEntry>({ max: 1000 });
+
+export function cachified<Value>(
+  options: Omit<CachifiedOptions<Value>, "cache">
+) {
+  return baseCachified({
+    cache: lruCacheAdapter(lru),
+    ...options
+  });
+}
